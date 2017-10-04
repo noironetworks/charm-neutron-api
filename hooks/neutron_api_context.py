900d6eea630064c7d82359f16a1eba28b5f95bea
Ratnakar Kolli <rkolli@noironetworks.com>
2017-10-04 04:57:10 +0000

    ACI Aim support

--- hooks/neutron_api_context.py
+++ hooks/neutron_api_context.py
@@ -308,6 +308,13 @@ class NeutronCCContext(context.NeutronContext):
                 else:
                     ctxt['path_mtu'] = config('global-physnet-mtu')
 
+        if config('neutron-plugin') == 'aci':
+            identity_context = IdentityServiceContext(service='neutron',
+                                    service_user='neutron')()
+            if identity_context is not None:
+                ctxt.update(identity_context)
+            ctxt.update(CiscoAciContext()())
+
         return ctxt
 
 
@@ -448,3 +455,22 @@ class MidonetContext(context.OSContextGenerator):
                 if self.context_complete(ctxt):
                     return ctxt
         return {}
+
+class CiscoAciContext(context.OSContextGenerator):
+    def __init__(self, rel_name='neutron-plugin-api-subordinate'):
+        self.rel_name = rel_name
+        self.interfaces = [rel_name]
+
+    def __call__(self):
+        for rid in relation_ids(self.rel_name):
+            for unit in related_units(rid):
+                rdata = relation_get(rid=rid, unit=unit)
+                plugin = rdata.get('neutron_plugin')
+                if str(plugin) == "aci":
+                    ctxt = {}
+                    for k in rdata.keys():
+                        ctxt[k] = rdata.get(k)
+                    if self.context_complete(ctxt):
+                        return ctxt
+        return {}
+
