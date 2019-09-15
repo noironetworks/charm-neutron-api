3523200f23b28ab5cc17b4ea26f5688e3cea1c8b
Ratnakar Kolli <rkolli@gmail.com>
2019-09-15 00:14:02 -0700

    Aci plugin support

--- hooks/neutron_api_context.py
+++ hooks/neutron_api_context.py
@@ -596,6 +596,13 @@ class NeutronCCContext(context.NeutronContext):
 
             ctxt['service_plugins'] = ','.join(ctxt['service_plugins'])
 
+        if config('neutron-plugin') == 'aci':
+            identity_context = IdentityServiceContext(service='neutron',
+                                                      service_user='neutron')()
+            if identity_context is not None:
+                ctxt.update(identity_context)
+            ctxt.update(CiscoAciContext()())
+
         return ctxt
 
 
@@ -879,3 +886,22 @@ class DesignateContext(context.OSContextGenerator):
                 ctxt['ipv6_ptr_zone_prefix_size'] = (
                     config('ipv6-ptr-zone-prefix-size'))
         return ctxt
+
+
+class CiscoAciContext(context.OSContextGenerator):
+    def __init__(self, rel_name='neutron-plugin-api-subordinate'):
+        self.rel_name = rel_name
+        self.interfaces = [rel_name]
+
+    def __call__(self):
+        for rid in relation_ids(self.rel_name):
+            for unit in related_units(rid):
+                rdata = relation_get(rid=rid, unit=unit)
+                plugin = rdata.get('neutron_plugin')
+                if str(plugin) == "aci":
+                    ctxt = {}
+                    for k in rdata.keys():
+                        ctxt[k] = rdata.get(k)
+                    if self.context_complete(ctxt):
+                        return ctxt
+        return {}
